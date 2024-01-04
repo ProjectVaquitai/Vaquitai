2cf78072ed8b4f5a88bb418188fad9844231c3f0
Celestial <python.spider.08@gmail.com>
2024-01-04 16:08:03 +0800

    structure rewind

--- data_juicer/ops/deduplicator/__init__.py
+++ data_juicer/ops/deduplicator/__init__.py
@@ -1,3 +1,2 @@
-from .image import image_deduplicator
-from .text import (document_deduplicator, document_minhash_deduplicator,
+from . import (image_deduplicator, document_deduplicator, document_minhash_deduplicator,
                document_simhash_deduplicator)
